1cfb850d2223249b19bae6a5d468c3afd9ac589c
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2011-09-14 09:57:39 +0200

    Trivial syntax updates.

--- Sources/LambdaAlert.h
+++ Sources/LambdaAlert.h
@@ -1,4 +1,4 @@
-@interface LambdaAlert : NSObject {}
+@interface LambdaAlert : NSObject
 
 - (id) initWithTitle: (NSString*) title message: (NSString*) message;
 - (void) addButtonWithTitle: (NSString*) title block: (dispatch_block_t) block;
--- Sources/LambdaSheet.h
+++ Sources/LambdaSheet.h
@@ -1,4 +1,4 @@
-@interface LambdaSheet : NSObject {}
+@interface LambdaSheet : NSObject
 
 @property(copy) dispatch_block_t dismissAction;
 
