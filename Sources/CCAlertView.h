7bb98607fc37b961fa6766f09a66c4c35712e353
Ivan Zezyulya <ivan@zezyulya.com>
2012-10-31 14:01:25 +0700

    Add convenience methods to CCAlertView

--- Sources/CCAlertView.h
+++ Sources/CCAlertView.h
@@ -14,4 +14,17 @@ extern NSString *const CCAlertViewAnimatedKey;
 
 - (UITextField*) textFieldAtIndex: (NSInteger) textFieldIndex;
 
+// Convenience methods
++ (id) alertWithTitle:(NSString *)title message:(NSString *)message;
++ (id) alertWithTitle:(NSString *)title;
++ (id) alertWithMessage:(NSString *)message;
+
++ (void) showAlertWithTitle:(NSString *)title message:(NSString *)message block:(dispatch_block_t)block;
++ (void) showAlertWithTitle:(NSString *)title block:(dispatch_block_t)block;
++ (void) showAlertWithMessage:(NSString *)message block:(dispatch_block_t)block;
+
++ (void) showAlertWithTitle:(NSString *)title message:(NSString *)message;
++ (void) showAlertWithTitle:(NSString *)title;
++ (void) showAlertWithMessage:(NSString *)message;
+
 @end
